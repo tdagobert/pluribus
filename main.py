6868df3cd8126af6a248a1bf2a42af6a13cc3122
dagobert <tristan.dagobert@ens-paris-saclay.fr>
2025-04-08 19:51:01 +0200

    ...

--- main.py
+++ main.py
@@ -319,7 +319,7 @@ def load_images(cfg):
         print(f"{u_n}")
         im_n += [normalize_image(iio.read(join(cfg.dirout, u_n)), sat=0.01)]
     for i, u_n in enumerate(im_n):
-        iio.write(join(cfg.dirout, f"input_{i}.png"), u_n)
+        iio.write(join(cfg.dirout, f"input_{len(files)-i}.png"), u_n)
 
     iio.write(join(cfg.dirout, f"u_0.png"),
               normalize_image(iio.read(join(cfg.dirout, files[-2])), sat=0.01))
